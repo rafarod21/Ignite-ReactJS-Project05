226acd615c824389e7cf94f7e0439ae794535440
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2023-03-15 12:55:25 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/Ignite-ReactJS-Project05/'
+    }
+
+    return config
   }
 }
